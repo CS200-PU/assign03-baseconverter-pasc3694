466b6f9994a4234f1d57762a8449a6586ea77ac7
pasc3694 <143458629+pasc3694@users.noreply.github.com>
2024-09-17 18:36:48 +0000

    made two functions and headers

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,53 @@
 
 using namespace std;
 
-int main () {
+int hexCharToInt (char hexDigit);
+char getBase (const string& strNumber);
+string getNumber (const string& prompt);
+void printTitle (const string& myTitle); 
+string binaryToDecimal (const string& strNumber); 
+string decimalToBinary (const string& strNumber); 
+string decimalToHex (const string& strNumber); 
+string hexToDecimal (const string& strNumber); 
+string hexToBinary (const string& strNumber); 
+string binaryToHex (const string& strNumber); 
 
-  cout << "Hello World" << endl;
+int main () {
+  string num = "0b10";
+  char first = 'F';
+  int value;
+
+  value = hexCharToInt (first);
+
+  first = getBase (num);
+
+  cout << first << ": " << value << endl; 
   
   return EXIT_SUCCESS;
+}
+
+int hexCharToInt (char hexDigit) {
+  int value = static_cast<int> (hexDigit);
+  if (64 < value && 71 > value) {
+    value -= 55;
+  }
+  else if (47 < value && 58 > value) {
+    value -= 48;
+  }
+  return value;
+}
+
+char getBase (const string &strNumber) {
+  char base = 'D';
+  if ('0' == strNumber[0] && 'x' == strNumber[1]) {
+    base = 'H';
+  }
+  else if ('0' == strNumber[0] && 'b' == strNumber[1]) {
+    base = 'B';
+  }
+  return base;
+}
+
+string getNumber (const string &prompt) {
+  
 }
