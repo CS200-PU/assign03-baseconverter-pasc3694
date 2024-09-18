5ab0ddeecfaeba16c93c0c5afeee2ef862c1c43b
pasc3694 <143458629+pasc3694@users.noreply.github.com>
2024-09-18 23:53:16 +0000

    made decimal to binary and binary to decimal

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 //******************************************************************************
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -24,7 +25,8 @@ string hexToBinary (const string& strNumber);
 string binaryToHex (const string& strNumber); 
 
 int main () {
-  string num = "0b10";
+  string num = "153";
+  string answer;
   char first = 'F';
   int value;
 
@@ -32,7 +34,9 @@ int main () {
 
   first = getBase (num);
 
-  cout << first << ": " << value << endl; 
+  answer = decimalToBinary (num);
+
+  cout << first << ": " << answer << endl; 
   
   return EXIT_SUCCESS;
 }
@@ -60,5 +64,54 @@ char getBase (const string &strNumber) {
 }
 
 string getNumber (const string &prompt) {
+  string number;
+  cout << prompt;
+  cin >> number;
+  return number;
+}
+
+void printTitle (const string &myTitle) {
+  const string STAR_LINE = "**************************************";
+  const string STARS = "*****";
+  cout << STAR_LINE << endl
+       << STARS << myTitle << STARS << endl
+       << STAR_LINE << endl;
+}
+
+string binaryToDecimal (const string& strNumber) {
+  double digits = strNumber.length() - 2;
+  double decimalNum = 0;
+  for (double index = 0; index < strNumber.length() - 2; index++) {
+    if (strNumber[index + 2] == '1') {
+     decimalNum += (pow (2, digits - index - 1));
+    }
+  } 
+
+  return to_string (static_cast<int> (decimalNum));
+}
+
+string decimalToBinary (const string& strNumber) {
+  string binaryNum = "0b";
+  string binary = "";
+  int decimal = stoi (strNumber);
+
+  while (decimal != 0) {
+    if (decimal % 2 == 1) {
+      binary.append ("1");
+    }
+    else{ 
+      binary.append ("0");
+    }
+    decimal /= 2;
+  }
+
+  for (int index = binary.length(); -1 < index; index--) {
+    binaryNum += binary[index];
+  }
+
+  return binaryNum;
+}
+
+string decimalToHex (const string& strNumber) {
   
 }
